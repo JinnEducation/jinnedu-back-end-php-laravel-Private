714bd07453deac042938a007c1c62db4036ccb21
Al-Saeyd J Al-Akhras (السيد الاخرسي) <79516145+alsaeyd-alakhras@users.noreply.github.com>
2026-01-29 17:14:50 +0200

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -337,14 +337,14 @@
                                         class="w-5 h-5 text-gray-500 group-hover:text-primary-600 transition-colors"
                                         fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026
-                                                 m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6
-                                                 a2.25 2.25 0 002.227 1.932H19.05
-                                                 a2.25 2.25 0 002.227-1.932l.857-6
-                                                 a2.25 2.25 0 00-1.883-2.542
-                                                 m-16.5 0V6A2.25 2.25 0 016 3.75h3.879
-                                                 a1.5 1.5 0 011.06.44l2.122 2.12
-                                                 a1.5 1.5 0 001.06.44H18
-                                                 A2.25 2.25 0 0120.25 9v.776" />
+                                                     m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6
+                                                     a2.25 2.25 0 002.227 1.932H19.05
+                                                     a2.25 2.25 0 002.227-1.932l.857-6
+                                                     a2.25 2.25 0 00-1.883-2.542
+                                                     m-16.5 0V6A2.25 2.25 0 016 3.75h3.879
+                                                     a1.5 1.5 0 011.06.44l2.122 2.12
+                                                     a1.5 1.5 0 001.06.44H18
+                                                     A2.25 2.25 0 0120.25 9v.776" />
                                     </svg>
 
                                     <!-- Balance -->
@@ -516,10 +516,10 @@
                         <i class="w-5 text-center fas fa-video text-primary-600 me-2"></i>
                         <span>{{ label_text('global', 'site.Online-Group-Classes', __('site.Online Group Classes')) }}</span>
                     </a>
-                    <a href="{{ route('site.group_class_details', ['id' => 1]) }}"
+                    <a href="{{ route('site.online_private_classes') }}"
                         class="flex items-center px-3 py-2 text-gray-600 rounded-lg transition-colors duration-200 nav-mobile-link hover:text-primary-600 hover:bg-gray-50">
                         <i class="w-5 text-center fas fa-users text-primary-600 me-2"></i>
-                        <span>{{ label_text('global', 'site.Group-Classes', __('site.Group Classes')) }}</span>
+                        {{ label_text('global', 'site.Private-Classes', __('site.Private Classes')) }}
                     </a>
                 </div>
             </div>
@@ -667,14 +667,14 @@
                                     class="w-5 h-5 text-gray-500 group-hover:text-primary-600 transition-colors"
                                     fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026
-                                                 m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6
-                                                 a2.25 2.25 0 002.227 1.932H19.05
-                                                 a2.25 2.25 0 002.227-1.932l.857-6
-                                                 a2.25 2.25 0 00-1.883-2.542
-                                                 m-16.5 0V6A2.25 2.25 0 016 3.75h3.879
-                                                 a1.5 1.5 0 011.06.44l2.122 2.12
-                                                 a1.5 1.5 0 001.06.44H18
-                                                 A2.25 2.25 0 0120.25 9v.776" />
+                                                     m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6
+                                                     a2.25 2.25 0 002.227 1.932H19.05
+                                                     a2.25 2.25 0 002.227-1.932l.857-6
+                                                     a2.25 2.25 0 00-1.883-2.542
+                                                     m-16.5 0V6A2.25 2.25 0 016 3.75h3.879
+                                                     a1.5 1.5 0 011.06.44l2.122 2.12
+                                                     a1.5 1.5 0 001.06.44H18
+                                                     A2.25 2.25 0 0120.25 9v.776" />
                                 </svg>
 
                                 <!-- Balance -->
